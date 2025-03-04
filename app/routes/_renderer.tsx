5796729742e897a5ebe59b86522558e28e90abf8
ShintaroMorimoto <morimoto-s1@gicloud.co.jp>
2025-03-04 11:33:31 +0900

    ファビコン追加

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -7,7 +7,7 @@ export default reactRenderer(({ children }) => {
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/gemini-color.svg' />
         <Link href='/app/style.css' rel='stylesheet' />
         <Script src='/app/client.ts' async />
       </head>
